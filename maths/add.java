b9c2965cc0457cbfac11463a0ddf4e58d74edf81
haakaashs <haakaash10@gmail.com>
2023-05-13 20:13:05 +0530

    math library

---
+++ maths/add.java
@@ -0,0 +1,13 @@
+package maths;
+
+
+public class add {
+    
+    public static void main(String []args) {
+        // int c =0;
+        System.out.println("haakaash");
+    }
+    
+}
+
+// curl -fsSL -o install.sh https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
